16d08d49b0e1b34436a01d611bcebbae57a0459d
createhardware <video.semen@gmail.com>
2023-06-15 03:35:46 +1000

    edit commands & kill docker if create apk file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+import os
+import time
+
 import subprocess
 
 import asyncio
@@ -14,7 +17,7 @@ TOKEN = ""
 
 USERID = ""
 
-workdir = r"C:\\"
+workdir = r"H:\arduino_work_proj\MyHouse_kivy_app"
 
 try:
     f = open("token.txt", "r")
@@ -33,18 +36,24 @@ except FileNotFoundError:
     pass
 
 
-command_update = 'dir > 1.txt'
-command_build = 'dir > 2.txt'
+command_update = 'git pull'
+
+command_build = r'docker run --rm --cpus 10 -v %cd%:/home/user/buildozer/ -v C:\buildozer:/home/user/.buildozer/ queirozt/kivy-buildozer buildozer android debug'
+
 command_get = 'dir > 3.txt'
 
 proc_update = None
 proc_build = None
 proc_get = None
 
+old_filetime = 0
+build_start_time = 0
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+def get_filetime():
+    return time.time() - os.path.getmtime(workdir + r"\bin\myhouse-0.1-arm64-v8a_armeabi-v7a-debug.apk")
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -67,10 +76,16 @@ async def build(message: types.Message):
         return
     
     if not proc_build:
+        global old_filetime 
+        global build_start_time
+        old_filetime = get_filetime()
+        build_start_time = time.time()
+
         proc_build = subprocess.Popen(
             (command_build),
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
             shell=True,
             cwd=workdir
         )
@@ -108,14 +123,18 @@ async def get_file(message: types.Message):
     if USERID !=  message.chat.id:
         return    
     
-    if not proc_get:
-        proc_get = subprocess.Popen(
-            (command_get),
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            shell=True,
-            cwd=workdir
-        )
+    # if not proc_get:
+    #     proc_get = subprocess.Popen(
+    #         (command_get),
+    #         stdin=subprocess.PIPE,
+    #         stdout=subprocess.PIPE,
+    #         shell=True,
+    #         cwd=workdir
+    #     )
+
+    # if platform.system() == 'Windows':
+    print(get_filetime())
+
     print("get file")
 
 
@@ -146,11 +165,20 @@ async def sender():
                 await bot.send_message(chat_id, "build completed")
  
     
-        if proc_get:
-            if proc_get.poll() is not None:
-                proc_get = None
-                await bot.send_message(chat_id, "get completed")
-                
+        # if proc_get:
+        #     if proc_get.poll() is not None:
+        #         proc_get = None
+        #         await bot.send_message(chat_id, "get completed")
+
+
+        global old_filetime 
+        filetime = get_filetime()
+        if filetime < old_filetime:
+            if filetime > 10:
+                await bot.send_message(chat_id, "file created!\nTime: " + str(int(time.time() - build_start_time)) + " seconds")
+                proc_build.kill()
+                old_filetime = filetime 
+
 
 
 if __name__ == '__main__':
